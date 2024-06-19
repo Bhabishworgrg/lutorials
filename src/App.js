6e07c70a0f2774cb85f0a6a045891e59c9efaa36
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-06-19 17:03:46 +0545

    Setup Basic Skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationPane from './components/NavigationPane';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-auto p-0'>
+            <NavigationPane />
+          </div>
+          
+          <div className='col content overflow-auto'>
+            <div className='p-5'>
+              <Routes>
+                {/* <Route path='/page-name' element={<Page />} /> */}
+              </Routes>
+            </div>
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
